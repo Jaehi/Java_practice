a547acc27bea39038a5348c8d5efaf5ac5ec9bad
jungjaehi <damnyomoonwon1003@gmail.com>
2021-03-03 18:08:33 +0900

    문자열

--- src/TestString.java
+++ src/TestString.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class TestString {
+import java.util.Locale;
+
+public class TestString {
+    public static void main(String[] args) {
+        String a = "apple";
+        String b = "Apple";
+        String c = "apple";
+        String d = new String("apple");
+        String e = "choco heim";
+        String f = "CHOCO HAZELNUT";
+
+        System.out.println(a.equals(b));
+        System.out.println(c.equals(a));
+        System.out.println(c.equals(d));
+        System.out.println(c == d);
+        System.out.println(e.indexOf("heim"));
+        System.out.println(e.replaceAll("heim","Heim"));
+        System.out.println(e.substring(0,10));
+        System.out.println(e.toUpperCase());
+        System.out.println(f.toLowerCase());
+    }
 }
--- src/TestStringBuffer.java
+++ src/TestStringBuffer.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class TestStringBuffer {
+public class TestStringBuffer {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("hi");
+        sb.append(" ");
+        sb.append("nice to meet you");
+
+        String temp = "";
+        temp += "hi";
+        temp += " ";
+        temp += "yaku";
+
+        StringBuffer bs = new StringBuffer();
+        bs.append("apple");
+        bs.append(" ");
+        bs.append("banana");
+        bs.insert(3,"purple");
+        String some = bs.substring(3,9).toString();
+
+        System.out.println(sb);
+        System.out.println(sb.toString());
+        System.out.println(temp);
+
+        System.out.println(bs);
+        System.out.println(bs.toString());
+        System.out.println(some);
+    }
 }
