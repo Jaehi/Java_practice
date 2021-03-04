14fb3586e835007e43a4f843691b939da3c964f5
jungjaehi <damnyomoonwon1003@gmail.com>
2021-03-04 10:43:58 +0900

    조건문

---
+++ src/Condition1Demo.java
@@ -0,0 +1,7 @@
+public class Condition1Demo {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("result : true");
+        }
+    }
+}
---
+++ src/LoginDemo2.java
@@ -0,0 +1,16 @@
+public class LoginDemo2 {
+    public static void main(String[] args) {
+        String id = args[0];
+        String psw = args[1];
+
+        if (id.equals("egoing")){
+            if (psw.equals("010101")){
+                System.out.println("right");
+            }else{
+                System.out.println("wrong");
+            }
+        }else{
+            System.out.println("wrong");
+        }
+    }
+}
