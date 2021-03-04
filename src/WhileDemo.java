91da64cebaf9a853366029c33219c31abf815770
jungjaehi <damnyomoonwon1003@gmail.com>
2021-03-04 11:34:48 +0900

    반복문

---
+++ src/WhileDemo.java
@@ -0,0 +1,17 @@
+public class WhileDemo {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 10){
+            i++;
+            System.out.println("Nice"+i*2);
+        }
+
+//        while (true){
+//            System.out.println("Good");
+//        }
+        /* while (false){
+                System.out.println("Bad");
+         */
+    }
+}
