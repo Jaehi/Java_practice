fdabbd33443946d74d960c837496c891eb1a25df
jungjaehi <damnyomoonwon1003@gmail.com>
2021-04-09 23:29:01 +0900

    change

--- src/Collection/TreeSet/MemberTreeSetTest.java
+++ src/Collection/TreeSet/MemberTreeSetTest.java
@@ -9,7 +9,7 @@ public class MemberTreeSetTest {
 
         Member member1 = new Member(2,"돌고래");
         Member member2 = new Member(1,"바위고래");
-        Member member3 = new Member(3,"자갈고래");
+        Member member3 = new Member(3,"rkfwk고래");
 
         memberTreeSet.addMember(member1);
         memberTreeSet.addMember(member2);
